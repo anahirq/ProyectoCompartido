58725dc4a7bd3397c1bf7e087d18e5ba23ab700c
Anahi Rojas <anahirqsws@gmail.com>
2021-09-21 21:43:34 -0500

    Project base

---
+++ ProyectoCompartido/TabBar.swift
@@ -0,0 +1,35 @@
+//
+//  TabBar.swift
+//  ProyectoCompartido
+//
+//  Created by Anahi Rojas on 21/09/21.
+//
+
+import UIKit
+
+class TabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.tintColor = .systemBackground
+        navBarContent()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func navBarContent() {
+        let anahiNavController = UINavigationController(rootViewController: AnahiVC())
+        anahiNavController.tabBarItem.title = "Anahi"
+        anahiNavController.tabBarItem.image = #imageLiteral(resourceName: "capybara")
+        
+        
+        let alejandroNavController = UINavigationController(rootViewController: AlejandroVC())
+        alejandroNavController.tabBarItem.title = "Alejandro"
+        alejandroNavController.tabBarItem.image = #imageLiteral(resourceName: "raven")
+        
+        viewControllers = [anahiNavController, alejandroNavController]
+    }
+
+}
